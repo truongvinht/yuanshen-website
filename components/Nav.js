ae8ed37bb1e249744d68351b010ee9ef1df724b0
Truong Vinh Tran <truongvinht@gmail.com>
2021-11-06 17:58:36 +0100

    add navbar

---
+++ components/Nav.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import navStyles from '../styles/Nav.module.css'
+
+const Nav = () => {
+    return (
+        <nav className={navStyles.nav}>
+            <ul>
+                <li>
+                    <Link href='/'><a>Home</a></Link>
+                </li>
+                <li>
+                    <Link href='/about'><a>About</a></Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav
